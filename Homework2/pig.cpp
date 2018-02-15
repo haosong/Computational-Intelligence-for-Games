2fe9a5528e02d334bc8072e37ca8b717375d50d2
Hao Song <mail@shawnsong.com>
2018-02-15 13:22:59 -0500

    change from array to vector

--- Homework2/pig.cpp
+++ Homework2/pig.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 #include <sstream>
 #include <iomanip>
+#include <vector>
 
 #define LIMIT 99
 
-double ***prob;
-int **move;
-double **roll;
+std::vector<std::vector<std::vector<double> > > prob;
+std::vector<std::vector<int> > move;
+std::vector<std::vector<double> > roll;
 
 void get_all_roll_prob(int goal, int x, int y) {
     for (int target = 2; target <= std::max(2, goal - std::min(x, y)); target++) {
-        auto **prob = new double *[goal + 1];
-        for (int i = 0; i <= goal; i++)
-            prob[i] = new double[7];
+        std::vector<std::vector<double> > prob(goal + 1, std::vector<double>(7, 0.0));
         auto *prob_target = new double[target + 1];
         prob_target[0] = 1.0;
-        for (int i = 2; i <= target; i++)
+        for (int i = 1; i <= target; i++)
             prob_target[i] = 0.0;
         for (int i = 2; i <= target; i++) {
             for (int j = 2; j <= 6; j++)
@@ -24,15 +23,13 @@ void get_all_roll_prob(int goal, int x, int y) {
             prob[i][0] = prob_target[i];
             prob[i][6] = 1.0 - prob[i][0];
             for (int j = 1; j < 6; j++) {
-                int r = std::min(6, i + j);
-                while (r >= 2 && i > i + j - r && i + j - r >= 0) {
-                    prob[i][j] += prob_target[i + j - r] / 6.0;
-                    r--;
-                }
+                for (int k = std::min(6, i + j); k >= 2 && k > j; k--)
+                    prob[i][j] += prob_target[i + j - k] / 6.0;
                 prob[i][6] -= prob[i][j];
             }
         }
         roll[target] = prob[target];
+        delete[] prob_target;
     }
 }
 
@@ -52,11 +49,10 @@ void win_prob_non_recursive(int goal, int x, int y) {
                     }
                 } else {
                     for (int s = 2; s <= std::max(2, goal - j); s++) {
-                        double p = prob[n - 1][i][j] * roll[s][6];
+                        double one_prob = prob[n - 1][i][j] * roll[s][6];
                         for (int k = s; k < s + 6; k++)
-                            p += (j + k >= goal ? 0.0 : prob[n - 1][i][j + k]) * roll[s][k - s];
-                        opt_prob = std::min(p, opt_prob);
-                        opt_move = p == opt_prob ? s : opt_move;
+                            one_prob += (j + k >= goal ? 0.0 : prob[n - 1][i][j + k]) * roll[s][k - s];
+                        opt_prob = std::min(one_prob, opt_prob);
                     }
                 }
                 prob[n][i][j] = opt_prob;
@@ -69,24 +65,15 @@ void win_prob_non_recursive(int goal, int x, int y) {
 int main(int argc, char **argv) {
     try {
         if (argc == 4) {
-            int x, y, goal;
+            int x = 0, y = 0, goal = 0;
             std::string str = std::string(argv[1]) + " " + std::string(argv[2]) + " " + std::string(argv[3]);
             std::istringstream iss(str);
             iss >> goal >> x >> y;
             if (!iss || goal < 0 || x < 0 || y < 0) throw -1;
-            prob = new double **[LIMIT];
-            for (int i = 0; i < LIMIT; i++) {
-                prob[i] = new double *[goal];
-                for (int j = 0; j < goal; j++) {
-                    prob[i][j] = new double[goal];
-                    for (int k = 0; k < goal; k++)
-                        prob[i][j][k] = 0.5;
-                }
-            }
-            move = new int *[goal];
-            for (int i = 0; i < goal; i++)
-                move[i] = new int[goal];
-            roll = new double *[std::max(2, goal - std::min(x, y)) + 1];
+            prob = std::vector<std::vector<std::vector<double> > >(LIMIT, 
+                std::vector<std::vector<double> >(goal, std::vector<double>(goal, 0.5)));
+            move = std::vector<std::vector<int> >(goal, std::vector<int>(goal, 0));
+            roll = std::vector<std::vector<double> >(std::max(2, goal - std::min(x, y)) + 1, std::vector<double>(7, 0.0));
             get_all_roll_prob(goal, x, y);
             win_prob_non_recursive(goal, x, y);
             std::cout << std::setprecision(6) << prob[LIMIT - 1][x][y] << " " << move[x][y] << std::endl;
